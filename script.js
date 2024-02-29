d2899b6ef5e3d5511a4d41f779d62b19488892cb
Cristian Centeno <centenosx@gmail.com>
2024-02-28 21:08:10 -0500

    Modificacion de pagina web se agregaron 2 logos y efectos para el scroll

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-
-
 window.addEventListener("scroll", function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
-})
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY > 0);
+
+    // Obtén las referencias de ambas imágenes de logo
+    var logo1 = document.getElementById("logo1");
+    var logo2 = document.getElementById("logo2");
+
+    // Alterna la visibilidad de los logos
+    if (window.scrollY > 0) {
+        logo1.classList.add("hidden");
+        logo2.classList.remove("hidden");
+    } else {
+        logo1.classList.remove("hidden");
+        logo2.classList.add("hidden");
+    }
+});
